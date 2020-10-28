5f2b12bfaa846eb4fab59d355e759077d6fd95d9
queezzz <queeniezeng49@gmail.com>
2020-10-28 01:11:18 -0400

    small changes

--- R/tmb.R
+++ R/tmb.R
@@ -1,29 +1,20 @@
-library(Deriv)
+## HOW TO MAKE THIS REQUIRE PACKAGE
+usethis::use_package("Deriv")
+
 
 
 # List of log-lik function for different ditributions
 loglik_list <- list(
   dpois = list(expr=expression(x * log(lambda) - lambda - lfactorial(x)),
                params=c("lambda")),
-  dnorm = list(expression(-log(2*pi*sd) - (x-mean)^2/(2*sd^2)),
+  dnorm = list(expr=expression(-log(2*4*(4*atan(1/5)-atan(1/239)))/2-log(sd) - (x-mean)^2/(2*sd^2)),
                params=c("mean","sd"))
 )
 
 
-# for testing
-form <- y ~ dpois(lambda = b1 * latitude^2)
-set.seed(101)
-dd <- data.frame(
-  y = rpois(100, lambda = 2),
-  latitude = rnorm(100)
-)
+#### need to convert pi as a build-in constant??? what if pi is a parameter
+### take care of maybe the e constant
 
-
-
-## form -> objective function (i.e. a function that computes -sum(dpois(y, lambda=b0*latitude^2))
-##  where the variables in the formula are *evaluated* in an environment that
-##  includes the current parameter
-##  values and information stored in a 'data' variable
 
 
 ## data frame lat, long
@@ -31,17 +22,21 @@ y ~ dpois(exp(log_lambda), ...,
           parameters = list(log_lambda = ~ poly(lat, long, 2))
 )
 
+#' @export
 mkfun <- function(formula, data) {
-  RHS <- formula[[3]]
-  response <- formula[[2]]
-  ddistn <- as.character(RHS[[1]]) ## get the name of distribution variable
-  arglist <- as.list(RHS[-1]) ## delete function name
+  if(missing(data)) {
+    stop(paste("missing data...")) # if no data
+    }
+  RHS <- formula[[3]] # dpois(lambda = (b0 * latitude^2))
+  response <- formula[[2]] #y
+  ddistn <- as.character(RHS[[1]]) ## dpois /// get the name of distribution variable
+  arglist <- as.list(RHS[-1]) ## $lambda = (b0 * latitude^2) ///delete function name
   arglist1 <- c(
     list(x = response),
     arglist, ##
     list(log = TRUE)
   )
-  fn <- function(pars) {
+  fn <- function(pars) { ## parameter
     pars_and_data <- c(as.list(pars), data)
     r <- with(
       pars_and_data,
@@ -60,19 +55,19 @@ mkfun <- function(formula, data) {
     mnames <- loglik_list[[ddistn]]$params
     ## ???
     ## setdiff(all.vars(LL), "x")  ## response var should be the only non-parameter
-    d0 <- Deriv(LL, mnames)
+    d0 <- Deriv::Deriv(LL, mnames)
     ## evaluate all of the arguments to the log-likelihood
-    arglist_eval <- lapply(arglist, eval, pars_and_data)
+    arglist_eval <- lapply(arglist, eval, pars_and_data) ##lambda
     ## evaluate response variable and assign its value to 'x'
-    arglist_eval$x <- eval(response, pars_and_data)
+    arglist_eval$x <- eval(response, pars_and_data) #x = y
     ## derivative of log-lik wrt PDF parameters
-    d1 <- eval(d0, arglist_eval)
+    d1 <- eval(d0, arglist_eval) ## sub back to d0
     ## compute the deriv of log_lik with respect to its parameters
     parnames <- setdiff(all.vars(RHS), names(data))
     dlist <- list()
     glist <- list()
     for (p in parnames) {
-      dlist[[p]] <- eval(Deriv(arglist$lambda, p), pars_and_data)
+      dlist[[p]] <- eval(Deriv::Deriv(arglist$lambda, p), pars_and_data)
       glist[[p]] <- -sum(d1 * dlist[[p]])
     }
     return(unlist(glist))
