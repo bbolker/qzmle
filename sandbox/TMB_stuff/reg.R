d1088936d38ae7aea05655e8319a104774e9f05f
Ben Bolker <bbolker@gmail.com>
2021-01-21 15:28:14 -0500

    bmb meeting

--- sandbox/TMB_stuff/reg.R
+++ sandbox/TMB_stuff/reg.R
@@ -21,3 +21,26 @@ lm(y~x) ## check if its the same as lm
 ## check if its the same as optim and nlminb
 opt <- optim(par = obj$par, fn = obj$fn, gr=obj$gr)
 opt2 <- nlminb(start = obj$par, obj = obj$fn, gr = obj$gr)
+
+
+compile("reg_vec.cpp")
+dyn.load(dynlib("reg_vec"))
+
+
+obj2 <- MakeADFun(
+  data = list(x = x, y = y),
+  parameters = list(b0 = 0, b1 = 0, log_sigma = 0),
+  DLL = "reg_vec")
+
+
+with(obj2, nlminb(start = par, obj = fn, gr=gr))
+
+dnbinom(x=0:5, mu=1:6, size=rep(0.2,6), log=TRUE)
+
+
+mle2(y ~ dnorm(b0+b1*x, exp(log_sigma)),
+     start=list(b0=0,b1=0,log_sigma=0),
+     data=list(x=rnorm(5),y=rnorm(5)))
+## -> create reg_vec.cpp
+## compile it
+## optimize it
