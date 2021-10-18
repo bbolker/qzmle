44a2419df688d0f6cc79751011f1aed758941229
Ben Bolker <bbolker@gmail.com>
2021-10-18 12:52:01 -0400

    examples

---
+++ examples.R
@@ -0,0 +1,98 @@
+## examples to make work with qzmle
+library(qzmle)
+library(bbmle)
+
+## examples from ?bbmle::mle2
+
+d <- data.frame(x = 0:10,
+               y = c(26, 17, 13, 12, 20, 5, 9, 8, 5, 4, 8))
+attach(d) ## !!!!
+## in general it is best practice to use the `data' argument,
+##  but variables can also be drawn from the global environment
+LL <- function(ymax=15, xhalf=6)
+  -sum(stats::dpois(y, lambda=ymax/(1+x/xhalf), log=TRUE))
+## uses default parameters of LL
+## data from global environment
+fit <- mle2(LL)
+fitqz <- qzmle::mle(LL) ## missing data ...
+## TASK 1. (a) Document the fact that qzmle::mle() doesn't work
+## with variables taken from the global environment
+## (b) Add an issue to possibly implement this ...
+## There's a legitimate argument for **not** implementing this.
+
+## TASK 2. (a) Document that qzmle::mle() doesn't work with user-specified NLL functions. (b) Add an issue to implement this.
+fit <- mle2(LL, data=d)
+fitqz <- qzmle::mle(LL, data = d)
+
+fit0 <- mle2(y~dpois(lambda=ymean),start=list(ymean=mean(d$y)),data=d)
+fit0qz <- qzmle::mle(y~dpois(lambda=ymean),
+                     start=list(ymean=mean(d$y)),
+                     data=d)
+optimHess(coef(fit0qz), fn = fit0qz$minuslogl)
+all.equal(coef(fit0), coef(fit0qz))
+all.equal(vcov(fit0), vcov(fit0qz), tol=1e-5)
+## TASK: return the negative log-likelihood _function_ as one component of a fitted mle object.
+
+
+## TASK: improve log likelihood method
+## FROM BBMLE:
+
+## function (object, ...) {
+##     if (length(list(...)))
+##         warning("extra arguments discarded")
+##     ## get negative log-likelihood
+##     val <- -object@min
+##     ## get df/number of coefficients
+##     attr(val, "df") <- length(object@coef)
+##     ## get number of observations
+##     attr(val, "nobs") <- attr(object, "nobs")
+##     class(val) <- "logLik"
+##     val
+## }
+
+## don't print, just return an appropriately structured object of
+## class "logLik" ...
+
+## anova(fit0,fit)
+summary(fit)
+logLik(fit)
+vcov(fit)
+
+fit1qz <- qzmle::mle(y~dpois(lambda=exp(lymax)/(1+x/exp(lhalf))),
+      start=list(lymax=0,lhalf=0),
+      data=d,
+      parameters=list(lymax~1,lhalf~1))
+
+## FIXME
+fit1qz_tmb <- qzmle::mle(y~dpois(lambda=exp(lymax)/(1+x/exp(lhalf))),
+      start=list(lymax=0,lhalf=0),
+      data=d,
+      parameters=list(lymax~1,lhalf~1),
+      method="TMB")
+
+fit1bb <- mle2(y~dpois(lambda=exp(lymax)/(1+x/exp(lhalf))),
+      start=list(lymax=0,lhalf=0),
+      data=d,
+      parameters=list(lymax~1,lhalf~1))
+all.equal(coef(fit1bb), coef(fit1qz), tol=1e-8)
+
+library(waldo)
+compare(coef(fit1bb), coef(fit1qz))
+
+set.seed(1001)
+lymax <- c(0,2)
+lhalf <- 0
+x <- sort(runif(200))
+g <- factor(sample(c("a","b"),200,replace=TRUE))
+y <- rnbinom(200,mu=exp(lymax[g])/(1+x/exp(lhalf)),size=2)
+d2 <- data.frame(x,g,y)
+
+fit3bb <- mle2(y~dnbinom(mu=exp(lymax)/(1+x/exp(lhalf)),size=exp(logk)),
+             parameters=list(lymax~g),data=d2,
+             start=list(lymax=0,lhalf=0,logk=0))
+
+## FIXME!
+fit3qz <- qzmle::mle(y~dnbinom(mu=exp(lymax)/(1+x/exp(lhalf)),
+                               size=exp(logk)),
+                     parameters=list(lymax~g), data=d2,
+                     start=list(lymax=0,lhalf=0,logk=0))
